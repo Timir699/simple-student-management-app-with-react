37011c53f021afa2610d326584a688921feff997
Islam Timir <islam.timir772@gmail.com>
2021-05-20 15:22:21 +0600

    Added some styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './App.scss';
+import AllList from './components/AllList'
+import Present from './components/Present'
+import Absent from './components/Absent'
+import shortid from 'shortid'
 
 function App() {
+  const [inputStudent, setInputStudent] = useState({
+    student : '',
+    faculty : '',
+    id : shortid.generate()
+  })
+
+  const changeHand = (e) => {
+    setInputStudent({
+      ...inputStudent,
+      [e.target.name] : e.target.value
+    })
+  }
+  const [allData, setAllData] = useState([])
+  const [presentData, setPresentData] = useState([])
+  const [absentData, setAbsentData] = useState([])
+  const [changeBtn,setChangeBtn ] = useState(false)
+  const [editableStudent, setEditableStudent] = useState(null)
+  
+  const addData = (e) => {
+    e.preventDefault() 
+    if (inputStudent.student && inputStudent.faculty) {
+      setAllData([...allData,inputStudent])
+      setInputStudent({
+        student : '',
+        faculty : '',
+        id : shortid.generate()
+      }) 
+    } else {
+        alert("Please provide valid data")
+    }
+    
+  }
+  const updateData = (e) => {
+    e.preventDefault()
+    if(!editableStudent) return
+    const upDatedData = allData.find((item) => {
+      return item.id == editableStudent.id
+    })
+    upDatedData.student = inputStudent.student
+    upDatedData.faculty = inputStudent.faculty
+    setEditableStudent(null)
+    setInputStudent({
+      student : '',
+      faculty : '',
+      id : shortid.generate()
+    })
+    setChangeBtn(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className ="input-field">
+        <form className ="input-field__form" action="" onSubmit = { changeBtn ? updateData : addData }>
+          <input placeholder = "Enter Student Name" className ="input-field__form-input" type="text" name = 'student' value = {inputStudent.student} onChange = { (e) => changeHand(e)}/>
+          <select className ="input-field__form-select" name="faculty" id="" value={inputStudent.faculty} onChange = { (e) => changeHand(e) }>
+            <option className ="input-field__form-select--all">All</option>
+            <option className ="input-field__form-select--business" value="Business">Business</option>
+            <option className ="input-field__form-select--science" value="Science">Science</option>
+            <option className ="input-field__form-select--arts"  value="Arts">Arts</option>
+          </select>
+          <button className ="input-field__form-button">{changeBtn ? 'Update' : 'Add Data'}</button>
+        </form>
+      </div>
+      <div style = {{ display : "flex", justifyContent: 'space-evenly' }}>
+        <AllList 
+          allData = {allData}
+          setPresentData = {setPresentData}
+          presentData = {presentData}
+          setAbsentData = {setAbsentData}
+          absentData = {absentData}
+          setInputStudent = {setInputStudent}
+          setChangeBtn = {setChangeBtn}
+          setEditableStudent = {setEditableStudent}
+          setAllData = {setAllData}
+          />
+        <Present 
+           presentData = {presentData}
+           setPresentData = {setPresentData}
+           setAbsentData = {setAbsentData}
+           absentData = {absentData}
+         /> 
+        <Absent 
+            setAbsentData = {setAbsentData}
+            absentData = {absentData}
+            presentData = {presentData}
+            setPresentData = {setPresentData}
+        />
+        
+      </div>
+    </>
+
+    
   );
 }
 
---
+++ src/components/AllList.jsx
@@ -0,0 +1,56 @@
+const AllList = (props) => {
+    let {allData,setAllData,setPresentData,presentData,setAbsentData,absentData,setInputStudent,setChangeBtn,setEditableStudent} = props
+    let sendStudent = (student) => {
+        if ( presentData.find( (item) => item.id === student.id ) ) {
+            return alert("Allrady Present")
+        }
+        if ( absentData.find( (item) => item.id === student.id ) ) {
+            return alert("Already Added in absent")
+        }
+        setPresentData([...presentData, student])
+        
+    }
+    let sendAbsent = (student) => {
+        if ( absentData.find( (item) => item.id === student.id ) ) {
+            return alert("Allrady Absent")
+        }
+        if ( presentData.find( (item) => item.id === student.id ) ) {
+            return alert("Allrady Added in Present")
+        }
+        setAbsentData([...absentData, student])
+    }
+
+    const editList = (student) => {
+        setChangeBtn(true)
+        setInputStudent(student)
+        setEditableStudent(student)
+    }
+
+    const deleteHand = (student) => {
+        const filteredData = allData.filter( (item) => item.id !== student.id )
+        setAllData(filteredData)
+        const filteredPresentData = presentData.filter( (item) => item.id !== student.id )
+        setPresentData(filteredPresentData)
+        const filteredAbsentData = absentData.filter( (item) => item.id !== student.id )
+        setAbsentData(filteredAbsentData)
+    }
+    return ( 
+        <div className = "studentlist">
+            <h2 className = "studentlist__heading">All Students</h2>
+            {allData.map( (student) => {
+                return (
+                    <div className = "studentlist__student" key = {student.id}>
+                        <h2>{student.student}</h2>
+                        <h3>Dep: {student.faculty}</h3>
+                        <button className = "studentlist__student-btn " onClick = { () => sendStudent(student) }>Present</button>
+                        <button className = "studentlist__student-btn" onClick = { () => sendAbsent(student) }>Absent</button>
+                        <button className = "studentlist__student-btn" onClick = { () => editList(student) }>Edit</button>
+                        <button className = "studentlist__student-btn" onClick = { () => deleteHand(student)}>Delete</button>
+                    </div>
+                )
+            } )}
+        </div>
+     );
+}
+ 
+export default AllList;
